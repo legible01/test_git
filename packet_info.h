4b5a21e042a9f738ce205d4dc863a832f8473048
KKS <legible01@gmail.com>
2017-09-06 01:44:27 +0900

    direct stage area

--- packet_info.h
+++ packet_info.h
@@ -9,9 +9,7 @@ private:
 
     int param_count;
     uint8_t **sender_mac;//[param_count][6] //= new uint32_t[param_count][6];
-    uint32_t *sender_ip;//[param_count];
-    uint8_t **target_mac;//[param_count][6];//6 size of mac + argument number
-    uint32_t *target_ip;//[param_count];//save data with pton.
+
 
 public:
     void set_param_ip(int p_count,char *p_ip);
