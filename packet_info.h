e022a9922a397ea91ee7bf6b2a2ddfe38179e302
KKS <legible01@gmail.com>
2017-09-06 15:41:27 +0900

    new repo

--- packet_info.h
+++ packet_info.h
@@ -4,15 +4,8 @@
 #include "packet_info.h"
 class packet_info
 {
-private:
-    char *dev_name;
-
-    int param_count;
-    uint8_t **sender_mac;//[param_count][6] //= new uint32_t[param_count][6];
 
 
-public:
-    void set_param_ip(int p_count,char *p_ip);
 
 };
 
